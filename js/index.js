568dc4b734b71a37eca6e23e3d670bd3a5205a3e
NGUYEN DUC PHAT <ndp9a4@gmail.com>
2024-04-07 21:03:09 +0700

    shop do an

---
+++ js/index.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    $("nav_doc_toggler").click(function () {
+        console.log("cc");
+        $("#nav_doc").fade();
+        // $("#div2").fadeToggle("slow");
+        // $("#div3").fadeToggle(3000);
+    });
+});
